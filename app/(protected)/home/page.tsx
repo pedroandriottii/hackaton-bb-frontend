afc8f956feb58f97776474e743e9805d6dcc8b98
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-11-08 02:23:23 -0300

    fixing navbar and doing logout

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -8,11 +8,13 @@ const Discard: React.FC = () => {
     <div className='bg-bb-blue min-h-screen w-full flex flex-col'>
       <div className='p-4 flex-1 flex flex-col justify-around items-center'>
         <div>
-
           <p className='text-bb-yellow text-lg'>Bem vindo(a) ao</p>
           <h1 className=' text-4xl font-bold text-bb-yellow'>BB TechPoints</h1>
         </div>
-
+        <div className='bg-white flex flex-col p-4 rounded-xl gap-2'>
+          <button className='text-bb-blue text-xl'>Curiosidade</button>
+          <p>Em recife, <button className='font-bold'>todos os dias</button>, são geradas <button className='font-bold'>80 toneladas</button> de resíduo eletrônico. Isso equivale a 40 SUVs!</p>
+        </div>
         <div className='w-full max-w-md space-y-6 flex flex-col text-bb-blue text-xl'>
           <h2 className='text-white flex text-2xl mt-8 mb-4 justify-center '>O que deseja fazer hoje?</h2>
           <Link href={'/descartar'}>
--- components/base/navbar_logado.tsx
+++ components/base/navbar_logado.tsx
@@ -1,25 +1,64 @@
 'use client';
-import React from 'react';
-import { Coins} from 'lucide-react';
+import React, { useState } from 'react';
+import { Coins, User } from 'lucide-react';
 import { useUser } from '@/context/Usercontext';
 import Image from 'next/image';
+import Link from 'next/link';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
 
 const Navbar: React.FC = () => {
   const { user } = useUser();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    Cookies.remove('accessToken');
+    router.push('/login');
+  };
+
+  const toggleDialog = () => {
+    setIsDialogOpen(!isDialogOpen);
+  };
 
   return (
-    <div className='bg-bb-yellow flex p-4 items-center justify-between'>
-      <Image
-        src={"/logobb.svg"}
-        width={30}
-        height={30}
-        alt='Logo BB'
-      />
+    <div className='bg-bb-yellow flex p-4 items-center justify-between relative'>
+      <div
+        className='border border-bb-blue p-4 rounded-full text-bb-blue cursor-pointer'
+        onClick={toggleDialog}
+      >
+        <User />
+      </div>
+
+      <Link href={'/home'}>
+        <Image
+          src={"/logobb.svg"}
+          width={30}
+          height={30}
+          alt='Logo BB'
+        />
+      </Link>
 
       <div className='flex items-center text-bb-blue bg-yellow-300 p-2 rounded-full gap-2'>
         <Coins />
-        <p>{user ? user.points : 'Carregando...'}</p>
+        <p>{user ? user.points : '0'}</p>
       </div>
+
+      {isDialogOpen && (
+        <div className="absolute top-16 left-4 bg-white border border-gray-300 rounded shadow-lg p-4 w-48">
+          <>
+            <Link href="/profile" className="block py-2 text-blue-500 hover:underline">
+              Ver Perfil
+            </Link>
+            <button
+              onClick={handleLogout}
+              className="block w-full text-left py-2 text-red-500 hover:underline"
+            >
+              Logout
+            </button>
+          </>
+        </div>
+      )}
     </div>
   );
 };
