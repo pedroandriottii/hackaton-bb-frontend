58c61ec81c46a12f0c073d81b36027ca15c4f731
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-11-07 22:54:18 -0300

    Adding fonts and styling homepage

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,12 +1,49 @@
 import Navbar from '@/components/base/navbar';
+import { ChevronsRight, CircleDollarSign, Gift, MapPinned, Trash2 } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 const Discard: React.FC = () => {
   return (
-    <div className='bg-bb-blue'>
+    <div className='bg-bb-blue min-h-screen w-full flex flex-col'>
       <Navbar />
-      <div>
-        <h1 className='text-bb-yellow'>Descartar lixo eletrônico</h1>
+      <div className='p-4 flex-1 flex flex-col justify-around items-center'>
+        <div>
+
+          <p className='text-bb-yellow text-lg'>Bem vindo(a) ao</p>
+          <h1 className=' text-4xl font-bold text-bb-yellow'>BB TechPoints</h1>
+        </div>
+
+        <div className='w-full max-w-md space-y-6 flex flex-col text-bb-blue text-xl'>
+          <h2 className='text-white flex text-2xl mt-8 mb-4 justify-center '>O que deseja fazer hoje?</h2>
+          <Link href={'/descartar'}>
+            <div className='bg-bb-yellow rounded-xl p-4 flex flex-col items-center justify-center py-8'>
+              <div className='flex items-center gap-4'>
+                <Trash2 size={40} />
+                <ChevronsRight size={40} />
+                <CircleDollarSign size={40} />
+              </div>
+              <button className='mt-2 text-center'>Descartar itens por pontos</button>
+            </div>
+          </Link>
+
+          <Link href={'/procurar'}>
+            <div className='bg-bb-yellow rounded-xl p-4 flex flex-col items-center justify-center py-8'>
+              <MapPinned size={40} />
+              <button className='mt-2 text-center'>Pontos de descartes próximos</button>
+            </div>
+          </Link>
+
+          <Link href={'/recompensas'}>
+            <div className='bg-bb-yellow rounded-xl p-4 flex flex-col items-center justify-center py-8'>
+              <Gift size={40} />
+              <button className='mt-2 text-center'>Ver tabela de recompensas</button>
+            </div>
+          </Link>
+        </div>
+        <div>
+
+        </div>
       </div>
     </div>
   );
--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -4,8 +4,8 @@ import { Coins, User } from 'lucide-react';
 const Navbar: React.FC = () => {
   return (
     <div className='bg-bb-yellow flex p-4 items-center justify-between'>
-      <div className='bg-bb-blue rounded-full p-2'>
-        <User className='text-bb-yellow' />
+      <div className='border border-bb-blue rounded-full p-2'>
+        <User className='text-bb-blue' />
       </div>
       <h1>LogoBB</h1>
       <div className='flex items-center text-bb-blue bg-yellow-300 p-2 rounded-full gap-2'>
